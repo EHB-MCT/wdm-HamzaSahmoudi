f46dbb9dcca8aa073be3654ea5a54251a8487f11
Hamza Sahmoudi <hamza.sahmoudi@student.ehb.be>
2025-12-31 01:32:06 +0100

    Connect backend to mongodb via mongoose

--- DEV5/backend/backend.js
+++ DEV5/backend/backend.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo connected"))
+  .catch((err) => console.log("Mongo error", err));
 
 app.get("/status", (req, res) => {
   res.send("OK");
